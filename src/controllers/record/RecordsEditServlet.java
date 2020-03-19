cf4424528bcaf7b080bc6eb352dd3b57227d3b51
shohei.tajima <tshohei007@gmail.com>
2020-03-19 22:02:25 +0900

    Add Record edit

--- src/controllers/record/RecordsEditServlet.java
+++ src/controllers/record/RecordsEditServlet.java
@@ -1,19 +1,26 @@
 package controllers.record;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Record;
+import models.User;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class RecordsEditServlet
  */
 @WebServlet("/records/edit")
 public class RecordsEditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +29,25 @@ public class RecordsEditServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Record r = em.find(Record.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        User login_user = (User)request.getSession().getAttribute("login_user");
+        if(r != null && login_user.getId() == r.getUser().getId()) {
+            request.setAttribute("record", r);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("record_id", r.getId());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/records/edit.jsp");
+        rd.forward(request, response);
+    }
 
 }
